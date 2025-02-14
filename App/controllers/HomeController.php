8c7731ef627b3632665a4e064b66e39108a41da8
Tadiwanashe <20095319@tafe.wa.edu.au>
2025-02-15 00:38:03 +0800

    Create, read, Update & Delete Listings

--- App/controllers/HomeController.php
+++ App/controllers/HomeController.php
@@ -1,30 +1,26 @@
-<?php
-
-namespace App\Controllers;
-
 use Framework\Database;
 
 class HomeController
 {
-    protected $db;
+protected $db;
 
-    public function __construct()
-    {
-        $config = require basePath('config/db.php');
-        $this->db = new Database($config);
-    }
+public function __construct()
+{
+$config = require basePath('config/db.php');
+$this->db = new Database($config);
+}
 
-    /*
-     * Show the latest listings
-     *
-     * @return void
-     */
-    public function index()
-    {
-        $listings = $this->db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
+/*
+* Show the latest listings
+*
+* @return void
+*/
+public function index()
+{
+$listings = $this->db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
 
-        loadView('home', [
-            'listings' => $listings
-        ]);
-    }
+loadView('home', [
+'listings' => $listings
+]);
+}
 }
