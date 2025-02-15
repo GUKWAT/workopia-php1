e2ac8220f4c35285312ec27cbfd6aaf336b7c6f7
Tadiwanashe <20095319@tafe.wa.edu.au>
2025-02-15 11:19:59 +0800

    Register user

--- App/views/listings/create.view.php
+++ App/views/listings/create.view.php
@@ -2,44 +2,64 @@
 <?= loadPartial('navbar') ?>
 <?= loadPartial('top-banner') ?>
 
-
-    <!-- Job Listings -->
-    <section>
-        <div class="container mx-auto p-4 mt-4">
-            <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">All Jobs</div>
-            <?= loadPartial('message') ?>
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
-                <?php foreach ($listings as $listing) : ?>
-                    <!-- Job Listing 1: Software Engineer -->
-                    <div class="rounded-lg shadow-md bg-white">
-                        <div class="p-4">
-                            <h2 class="text-xl font-semibold"><?= $listing->title ?></h2>
-                            <p class="text-gray-700 text-lg mt-2">
-                                <?= $listing->description ?>
-                            </p>
-                            <ul class="my-4 bg-gray-100 p-4 rounded">
-                                <li class="mb-2"><strong>Salary:</strong> <?= formatSalary($listing->salary) ?></li>
-                                <li class="mb-2">
-                                    <strong>Location:</strong> <?= $listing->city ?>, <?= $listing->state ?>
-                                    <!-- <span class="text-xs bg-blue-500 text-white rounded-full px-2 py-1 ml-2">Local</span> -->
-                                </li>
-                                <?php if (!empty($listing->tags)) : ?>
-                                    <li class="mb-2">
-                                        <strong>Tags:</strong> <?= $listing->tags ?>
-                                    </li>
-                                <?php endif; ?>
-                            </ul>
-                            <a href="/listings/<?= $listing->id ?>" class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium text-indigo-700 bg-indigo-100 hover:bg-indigo-200">
-                                Details
-                            </a>
-                        </div>
-                    </div>
-                <?php endforeach; ?>
-            </div>
+    <section class="flex justify-center items-center mt-20">
+        <div class="bg-white p-8 rounded-lg shadow-md w-full md:w-600 mx-6">
+            <h2 class="text-4xl text-center font-bold mb-4">Create Job Listing</h2>
+            <form method="POST" action="/listings">
+                <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">
+                    Job Info
+                </h2>
+                <?= loadPartial('errors', [
+                    'errors' => $errors ?? []
+                ]) ?>
+                <div class="mb-4">
+                    <input type="text" name="title" placeholder="Job Title" class="w-full px-4 py-2 border rounded focus:outline-none" value="<?= $listing['title'] ?? '' ?>" />
+                </div>
+                <div class="mb-4">
+                    <textarea name="description" placeholder="Job Description" class="w-full px-4 py-2 border rounded focus:outline-none"><?= $listing['description'] ?? '' ?></textarea>
+                </div>
+                <div class="mb-4">
+                    <input type="text" name="salary" placeholder="Annual Salary" class="w-full px-4 py-2 border rounded focus:outline-none" value="<?= $listing['salary'] ?? '' ?>" />
+                </div>
+                <div class="mb-4">
+                    <input type="text" name="requirements" placeholder="Requirements" class="w-full px-4 py-2 border rounded focus:outline-none" value="<?= $listing['requirements'] ?? '' ?>" />
+                </div>
+                <div class="mb-4">
+                    <input type="text" name="benefits" placeholder="Benefits" class="w-full px-4 py-2 border rounded focus:outline-none" value="<?= $listing['benefits'] ?? '' ?>" />
+                </div>
+                <div class="mb-4">
+                    <input type="text" name="tags" placeholder="Tags" class="w-full px-4 py-2 border rounded focus:outline-none" value="<?= $listing['tags'] ?? '' ?>" />
+                </div>
+                <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">
+                    Company Info & Location
+                </h2>
+                <div class="mb-4">
+                    <input type="text" name="company" placeholder="Company Name" class="w-full px-4 py-2 border rounded focus:outline-none" value="<?= $listing['company'] ?? '' ?>" />
+                </div>
+                <div class="mb-4">
+                    <input type="text" name="address" placeholder="Address" class="w-full px-4 py-2 border rounded focus:outline-none" value="<?= $listing['address'] ?? '' ?>" />
+                </div>
+                <div class="mb-4">
+                    <input type="text" name="city" placeholder="City" class="w-full px-4 py-2 border rounded focus:outline-none" value="<?= $listing['city'] ?? '' ?>" />
+                </div>
+                <div class="mb-4">
+                    <input type="text" name="state" placeholder="State" class="w-full px-4 py-2 border rounded focus:outline-none" value="<?= $listing['state'] ?? '' ?>" />
+                </div>
+                <div class="mb-4">
+                    <input type="text" name="phone" placeholder="Phone" class="w-full px-4 py-2 border rounded focus:outline-none" value="<?= $listing['phone'] ?? '' ?>" />
+                </div>
+                <div class="mb-4">
+                    <input type="email" name="email" placeholder="Email Address For Applications" class="w-full px-4 py-2 border rounded focus:outline-none" value="<?= $listing['email'] ?? '' ?>" />
+                </div>
+                <button class="w-full bg-green-500 hover:bg-green-600 text-white px-4 py-2 my-3 rounded focus:outline-none">
+                    Save
+                </button>
+                <a href="/" class="block text-center w-full bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded focus:outline-none">
+                    Cancel
+                </a>
+            </form>
         </div>
     </section>
-
-
 
 <?= loadPartial('bottom-banner') ?>
 <?= loadPartial('footer') ?>
--- App/views/listings/index.view.php
+++ App/views/listings/index.view.php
@@ -1,12 +1,13 @@
 <?= loadPartial('head') ?>
 <?= loadPartial('navbar') ?>
-<?= loadPartial('showcase-search') ?>
 <?= loadPartial('top-banner') ?>
+
 
     <!-- Job Listings -->
     <section>
         <div class="container mx-auto p-4 mt-4">
-            <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">Recent Jobs</div>
+            <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">All Jobs</div>
+            <?= loadPartial('message') ?>
             <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
                 <?php foreach ($listings as $listing) : ?>
                     <!-- Job Listing 1: Software Engineer -->
@@ -34,12 +35,8 @@
                         </div>
                     </div>
                 <?php endforeach; ?>
-
             </div>
-            <a href="/listings" class="block text-xl text-center">
-                <i class="fa fa-arrow-alt-circle-right"></i>
-                Show All Jobs
-            </a>
+        </div>
     </section>
 
 
